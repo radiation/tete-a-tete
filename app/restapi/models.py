f63a2989280ab03e4a86947128417cdcbf856955
Bryan Choate <bryan.choate@gmail.com>
2023-11-21 14:55:33 -0500

    Initial (very) rough unit tests.

--- app/restapi/models.py
+++ app/restapi/models.py
@@ -3,14 +3,26 @@ from django.db import models
 class User(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    email_address = models.CharField(max_length=50)
+    email_address = models.CharField(max_length=100, unique=True)
+    user_name = models.CharField(default="", max_length=50, unique=True)
+    def save(self, *args, **kwargs):
+        if self.user_name == "":
+            self.user_name = self.first_name + "_" + self.last_name
+        super(User, self).save(*args, **kwargs)
+    def __str__(self):
+        return self.first_name + " " + self.last_name
 
 class Meeting(models.Model):    
     scheduler = models.ForeignKey(User, related_name="meetings_scheduler_related", on_delete=models.CASCADE)
     attendee = models.ForeignKey(User, related_name="meetings_attendee_related", on_delete=models.CASCADE)
+    title = models.CharField(default="", max_length=100)
     start_date = models.DateTimeField()
     end_date = models.DateTimeField()
     num_reschedules = models.IntegerField(default=0)
+    def save(self, *args, **kwargs):
+        if self.title == "":
+            self.title = str(self.scheduler) + " / " + str(self.attendee)+ " / " + str(self.start_date)
+        super(Meeting, self).save(*args, **kwargs)
 
 class ActionItem(models.Model):
     meeting = models.ForeignKey(Meeting, related_name="actionitems_meeting_related", on_delete=models.CASCADE)
---
+++ app/restapi/tests/test_urls.py
@@ -0,0 +1,4 @@
+import unittest
+from django.urls import resolve
+from restapi.urls import urlpatterns
+from restapi.views import *
