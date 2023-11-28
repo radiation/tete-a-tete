b1eef22e688f368a093bdb1d835482ea84036ce6
Bryan Choate <bryan.choate@gmail.com>
2023-11-28 10:25:16 -0500

    CustomUser progress, rebuild migrations

--- app/restapi/models.py
+++ app/restapi/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.translation import gettext_lazy as _
@@ -17,21 +18,9 @@ class CustomUser(AbstractUser):
     def __str__(self):
         return self.email
 
-class User(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    email_address = models.CharField(max_length=100, unique=True)
-    user_name = models.CharField(default="", max_length=50, unique=True)
-    def save(self, *args, **kwargs):
-        if self.user_name == "":
-            self.user_name = self.first_name.lower() + "_" + self.last_name.lower()
-        super(User, self).save(*args, **kwargs)
-    def __str__(self):
-        return self.first_name + " " + self.last_name
-
 class Meeting(models.Model):    
-    scheduler = models.ForeignKey(User, related_name="meetings_scheduler_related", on_delete=models.CASCADE)
-    attendee = models.ForeignKey(User, related_name="meetings_attendee_related", on_delete=models.CASCADE)
+    scheduler = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="meetings_scheduler_related", on_delete=models.CASCADE)
+    attendee = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="meetings_attendee_related", on_delete=models.CASCADE)
     title = models.CharField(default="", max_length=100)
     start_date = models.DateTimeField()
     end_date = models.DateTimeField()
@@ -43,7 +32,7 @@ class Meeting(models.Model):
 
 class ActionItem(models.Model):
     meeting = models.ForeignKey(Meeting, related_name="actionitems_meeting_related", on_delete=models.CASCADE)
-    assignee = models.ForeignKey(User, related_name="actionitems_assignee_related", on_delete=models.CASCADE)
+    assignee = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="actionitems_assignee_related", on_delete=models.CASCADE)
     completed = models.BooleanField()
     todo_item = models.TextField()
 
@@ -52,8 +41,8 @@ class Question(models.Model):
 
 class QuestionAnswer(models.Model):
     question = models.ForeignKey(Question, related_name="questionanswers_question_related", on_delete=models.CASCADE)
-    asker = models.ForeignKey(User, related_name="questions_asker_related", on_delete=models.CASCADE)
-    answerer = models.ForeignKey(User, related_name="questions_answerer_related", on_delete=models.CASCADE)
+    asker = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="questions_asker_related", on_delete=models.CASCADE)
+    answerer = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="questions_answerer_related", on_delete=models.CASCADE)
     answer_text = models.TextField()
     
 class AgendaItem(models.Model):
