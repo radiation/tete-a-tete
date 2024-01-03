fa6bccec6d35900da3245745522b896f9d3515c0
Bryan Choate <bryan.choate@gmail.com>
2024-01-03 16:14:30 -0500

    Fix copy/paste error

--- agendable/asgi.py
+++ agendable/asgi.py
@@ -5,7 +5,7 @@ from django.core.asgi import get_asgi_application
 
 from restapi import routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_celery_example.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'agendable.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
