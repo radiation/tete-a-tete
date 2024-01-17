2588ec9447a34ab2ed8de2cb9248aea25a718ba6
Bryan Choate <bryan.choate@gmail.com>
2024-01-17 15:23:03 -0500

    Fix health check bug

--- agendable/urls.py
+++ agendable/urls.py
@@ -15,7 +15,6 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path('health/', health, name='health'),
     path('admin/', admin.site.urls),
     path('api/', include('restapi.urls')),
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
