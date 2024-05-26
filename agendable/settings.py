3bba645f149162d8b541d86c280dd8c59a4e212b
Bryan Choate <bryan.choate@gmail.com>
2024-05-26 09:00:25 -0400

    checkpoint

--- agendable/settings.py
+++ agendable/settings.py
@@ -1,10 +1,12 @@
 from pathlib import Path
 from kombu import Queue
 import os
+import logging
+
+logger = logging.getLogger(__name__)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -184,6 +186,8 @@ LOGGING = {
     },
 }
 
+# Log the allowed hosts
+logger.debug(f"Allowed Hosts: {ALLOWED_HOSTS}")
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
@@ -202,7 +206,7 @@ USE_TZ = True
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+# SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 USE_X_FORWARDED_HOST = True
 SECURE_SSL_REDIRECT = False  # In development, True in production if applicable
 
