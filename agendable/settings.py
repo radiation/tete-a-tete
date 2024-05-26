9028e7d20cb584637cf24142d7ed1758b2c1cc83
Bryan Choate <bryan.choate@gmail.com>
2024-05-26 10:08:08 -0400

    Add ngrok tunnel and update allowed hosts

--- agendable/settings.py
+++ agendable/settings.py
@@ -15,9 +15,13 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = os.environ.get(
-    "DJANGO_ALLOWED_HOSTS", "localhost 127.0.0.1 [::1]"
-).split(" ")
+hosts = os.environ.get("DJANGO_ALLOWED_HOSTS", "localhost 127.0.0.1 [::1]").split(" ")
+
+if DEBUG:
+    hosts.append(".ngrok.io")
+
+ALLOWED_HOSTS = hosts
+print("Allowed Hosts:", ALLOWED_HOSTS)
 
 SITE_ID = 1
 
