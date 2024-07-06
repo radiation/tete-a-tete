6e2395e5672c2379006cd0caff9dcb879bff4fcb
Bryan Choate <bryan.choate@gmail.com>
2024-07-06 19:45:10 -0400

    Start removing DRF artifacts

--- backend/agendable/settings.py
+++ backend/agendable/settings.py
@@ -148,15 +148,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-REST_FRAMEWORK = {
-    "DEFAULT_PERMISSION_CLASSES": [
-        "rest_framework.permissions.IsAuthenticated",
-    ],
-    "DEFAULT_AUTHENTICATION_CLASSES": [
-        "rest_framework_simplejwt.authentication.JWTAuthentication",
-    ],
-}
-
 LOGGING = {
     "version": 1,  # Indicates dictConfig format
     "disable_existing_loggers": False,  # Do not disable existing loggers
